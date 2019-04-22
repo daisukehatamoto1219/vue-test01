7dd17fea9202e99a6ed906f5e44da740f37a8426
daisukehatamoto1219 <d.hata1219@gmail.com>
2019-04-22 11:20:20 +0900

    editForm

--- src/pages/editForm/Index.vue
+++ src/pages/editForm/Index.vue
@@ -2,24 +2,57 @@
   <v-container fluid fill-height>
     <v-layout>
       <v-flex>
-        <v-card>
-          <v-container fluid>
-            <v-layout justify-center align-center row wrap>
-              <v-flex>
-                <v-text-field v-model="message" label="message"/>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card>
-        <v-layout align-center="true">
-          <v-spacer />
-          <v-btn color="primary" @click="doUpdateMessage" :loading="loading">実行
-            <v-icon right dark>search</v-icon>
-          </v-btn>
-        </v-layout>
-        <v-layout align-center="true">
-          <p>{{ messageState }}</p>
-        </v-layout>
+        <v-stepper v-model="step">
+          <v-stepper-header>
+            <v-stepper-step step="1" :complete="step > 1">step1</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step step="2" :complete="step > 2">step2</v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step step="3" :complete="step > 3">step3</v-stepper-step>
+          </v-stepper-header>
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <v-card>
+                <v-container fluid>
+                  <v-layout justify-center align-center row wrap>
+                    <v-flex>
+                      <v-text-field v-model="message" label="message"/>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+              <v-layout align-center="true">
+                <v-spacer />
+                <v-btn color="primary" @click="doUpdateMessage" :loading="loading">確認
+                  <v-icon right dark>search</v-icon>
+                </v-btn>
+              </v-layout>
+            </v-stepper-content>
+            <v-stepper-content step="2">
+              <v-card>
+                <v-container fluid>
+                  <v-layout align-center="true">
+                    <p>{{ messageState }}</p>
+                  </v-layout>
+                </v-container>
+              </v-card>
+              <v-layout align-center="true">
+                <v-spacer />
+                <v-btn @click="step = 1">戻る
+                  <v-icon right dark>search</v-icon>
+                </v-btn>
+                <v-btn color="primary" @click="step = 3">決定
+                  <v-icon right dark>search</v-icon>
+                </v-btn>
+              </v-layout>
+            </v-stepper-content>
+            <v-stepper-content step="3">
+              <v-layout align-center="true">
+                <p>OK</p>
+              </v-layout>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
       </v-flex>
     </v-layout>
   </v-container>
@@ -30,6 +63,7 @@ export default {
   name: 'editForm',
   data: () => ({
     message: '',
+    step: 1,
     loading: false
   }),
   computed: {
@@ -41,7 +75,10 @@ export default {
     ...mapActions(['doUpdate']),
     doUpdateMessage () {
       this.loading = true
-      this.doUpdate(this.message).then(() => { this.loading = false })
+      this.doUpdate(this.message).then(() => {
+        this.step = 2
+        this.loading = false
+      })
     }
   },
   created () {
